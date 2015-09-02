40bc6127dd6de6fe0a0265ad77d3ae79351c5c50
yagodar <yagodarov.a.e@gmail.com>
2015-09-02 18:12:02 +0300

    Update BillList activity AppBar. Now search view expanded in landscape mode.

--- src/main/java/com/yagodar/android/bill_please/activity/bill_list/BillListActivity.java
+++ src/main/java/com/yagodar/android/bill_please/activity/bill_list/BillListActivity.java
@@ -1,5 +1,8 @@
 package com.yagodar.android.bill_please.activity.bill_list;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
@@ -38,30 +41,15 @@ public class BillListActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.actions_rest_activity_bill_list, menu);
 
-
         MenuItem menuItem = menu.findItem(R.id.action_rest_activiry_bill_list_search);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
 
-        searchView.setMaxWidth(5000);
+        searchView.setMaxWidth(Integer.MAX_VALUE);
 
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchableInfo searchableInfo =  searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(searchableInfo);
 
-        //SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        //SearchableInfo searchableInfo =  searchManager.getSearchableInfo(getComponentName());
-
-
-        //searchView.setSearchableInfo(searchableInfo);
-
-//        int searchPlateId = searchView.getContext().getResources().getIdentifier("android:id/search_plate", null, null);
-//        View searchPlate = searchView.findViewById(searchPlateId);
-//        if (searchPlate!=null) {
-//            searchPlate.setBackgroundColor(Color.DKGRAY);
-//            int searchTextId = searchPlate.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
-//            TextView searchText = (TextView) searchPlate.findViewById(searchTextId);
-//            if (searchText!=null) {
-//                searchText.setTextColor(Color.WHITE);
-//                searchText.setHintTextColor(Color.WHITE);
-//            }
-//        }
         return super.onCreateOptionsMenu(menu);
     }
 }
