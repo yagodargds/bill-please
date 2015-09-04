e3715e611376f1641829567d0ee326b43f686c15
yagodar <yagodarov.a.e@gmail.com>
2015-09-04 11:24:35 +0300

    Complete RecyclerView Adapter

--- src/main/java/com/yagodar/android/bill_please/activity/bill_list/BillListAdapter.java
+++ src/main/java/com/yagodar/android/bill_please/activity/bill_list/BillListAdapter.java
@@ -3,7 +3,6 @@ package com.yagodar.android.bill_please.activity.bill_list;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.BaseColumns;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -17,67 +16,19 @@ import com.yagodar.essential.model.ListModel;
 /**
  * Created by yagodar on 18.06.2015.
  */
-public class BillListAdapter extends AbsRecyclerViewAdapter<Bill> {
+public class BillListAdapter extends AbsRecyclerViewAdapter<Bill, BillListAdapter.ViewHolder> {
 
     public BillListAdapter(Context context, View.OnClickListener onClickListener, ListModel<Bill> listModel) {
         super(context, onClickListener, listModel);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
-
-        if (convertView == null) {
-            convertView = getLayoutInflater().inflate(R.layout.bill_row, null);
-
-            viewHolder = new ViewHolder();
-
-            viewHolder.textViewNumber = (TextView) convertView.findViewById(R.id.bill_number);
-            viewHolder.textViewName = (TextView) convertView.findViewById(R.id.bill_et_name);
-            viewHolder.textViewTaxVal = (TextView) convertView.findViewById(R.id.bill_tax_val);
-            viewHolder.textViewTaxType = (TextView) convertView.findViewById(R.id.bill_tax_type);
-            viewHolder.textViewTipVal = (TextView) convertView.findViewById(R.id.bill_tip_val);
-            viewHolder.textViewTipType = (TextView) convertView.findViewById(R.id.bill_tip_type);
-
-            viewHolder.buttonEdit = (Button) convertView.findViewById(R.id.bill_edit_button);
-            viewHolder.buttonEdit.setOnClickListener(getOnClickListener());
-
-            viewHolder.buttonRemove = (Button) convertView.findViewById(R.id.bill_remove_button);
-            viewHolder.buttonRemove.setOnClickListener(getOnClickListener());
-
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-
-        Bill bill = getItem(position);
-
-        viewHolder.textViewNumber.setText(String.valueOf(position + 1));
-        viewHolder.textViewName.setText(bill.getName());
-        viewHolder.textViewTaxVal.setText(bill.getFormattedTaxVal());
-        viewHolder.textViewTaxType.setText(bill.getTaxType().toString());
-        viewHolder.textViewTipVal.setText(bill.getFormattedTipVal());
-        viewHolder.textViewTipType.setText(bill.getTipType().toString());
-
-        Bundle buttonArgs = new Bundle();
-        buttonArgs.putLong(BaseColumns._ID, bill.getId());
-
-        viewHolder.buttonEdit.setTag(buttonArgs);
-        viewHolder.buttonRemove.setTag(buttonArgs);
-
-        return convertView;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(getLayoutInflater().inflate(R.layout.bill_row, parent, false), getOnClickListener());
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends AbsRecyclerViewAdapter.AbsViewHolder<Bill> {
 
-        private final TextView mTextView;
-
-        public ViewHolder(View v) {
-            super(v);
-        }
-    }
-
-    private static class ViewHolder {
         public TextView textViewNumber;
         public TextView textViewName;
         public TextView textViewTaxVal;
@@ -86,6 +37,40 @@ public class BillListAdapter extends AbsRecyclerViewAdapter<Bill> {
         public TextView textViewTipType;
         public Button buttonEdit;
         public Button buttonRemove;
+
+        public ViewHolder(View itemView, View.OnClickListener onClickListener) {
+            super(itemView);
+
+            textViewNumber = (TextView) itemView.findViewById(R.id.bill_number);
+            textViewName = (TextView) itemView.findViewById(R.id.bill_et_name);
+            textViewTaxVal = (TextView) itemView.findViewById(R.id.bill_tax_val);
+            textViewTaxType = (TextView) itemView.findViewById(R.id.bill_tax_type);
+            textViewTipVal = (TextView) itemView.findViewById(R.id.bill_tip_val);
+            textViewTipType = (TextView) itemView.findViewById(R.id.bill_tip_type);
+
+            buttonEdit = (Button) itemView.findViewById(R.id.bill_edit_button);
+            buttonEdit.setOnClickListener(onClickListener);
+
+            buttonRemove = (Button) itemView.findViewById(R.id.bill_remove_button);
+            buttonRemove.setOnClickListener(onClickListener);
+        }
+
+        @Override
+        public void onBind(Bill model, int position) {
+            textViewNumber.setText(String.valueOf(position + 1));
+            textViewName.setText(model.getName());
+            textViewTaxVal.setText(model.getFormattedTaxVal());
+            textViewTaxType.setText(model.getTaxType().toString());
+            textViewTipVal.setText(model.getFormattedTipVal());
+            textViewTipType.setText(model.getTipType().toString());
+
+            Bundle buttonArgs = new Bundle();
+            buttonArgs.putLong(BaseColumns._ID, model.getId());
+
+            buttonEdit.setTag(buttonArgs);
+            buttonRemove.setTag(buttonArgs);
+        }
+
     }
 
 }
