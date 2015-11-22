bba0e5a8ce0f70f793750b70f1689409c5fa6a3f
yagodar <yagodarov.a.e@gmail.com>
2015-11-22 20:59:15 +0300

    Working on improve activity cycles

--- src/main/java/com/yagodar/android/bill_please/activity/bill_list/BillListFragment.java
+++ src/main/java/com/yagodar/android/bill_please/activity/bill_list/BillListFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -43,6 +44,8 @@ public class BillListFragment extends AbsLoaderProgressRecyclerViewFragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, toString() + " onActivityResult() request="+requestCode+" result="+resultCode);
+
         switch(resultCode) {
             case Activity.RESULT_OK:
                 if (requestCode == BillPleaseLoaderFactory.BillLoaderType.APPEND_BILL.ordinal()) {
@@ -61,8 +64,24 @@ public class BillListFragment extends AbsLoaderProgressRecyclerViewFragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.d(TAG, toString() + " onStart()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.d(TAG, toString() + " onStop()");
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
+
+        Log.d(TAG, toString() + " onResume()");
 
         setAvailable(true);
 
@@ -144,4 +163,6 @@ public class BillListFragment extends AbsLoaderProgressRecyclerViewFragment {
     private Button mButtonBillAppend;
 
     private final static String RECYCLER_LAYOUT_PARCELABLE_TAG = "recycler_layout_parcelable";
+
+    public static final String TAG = BillListFragment.class.getSimpleName();
 }
