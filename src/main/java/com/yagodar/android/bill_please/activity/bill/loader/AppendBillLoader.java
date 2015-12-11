82d827925d44e0571966c5e49c598eaeb146c0d5
yagodar <yagodarov.a.e@gmail.com>
2015-12-11 12:12:04 +0300

    Refactor append loader

--- src/main/java/com/yagodar/android/bill_please/activity/bill/loader/AppendBillLoader.java
+++ src/main/java/com/yagodar/android/bill_please/activity/bill/loader/AppendBillLoader.java
@@ -2,11 +2,9 @@ package com.yagodar.android.bill_please.activity.bill.loader;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.provider.BaseColumns;
 import android.support.v4.os.CancellationSignal;
 
 import com.yagodar.android.bill_please.R;
-import com.yagodar.android.bill_please.model.Bill;
 import com.yagodar.android.bill_please.model.BillList;
 import com.yagodar.android.bill_please.model.BillOrder;
 import com.yagodar.android.bill_please.store.BillOrderRepository;
@@ -33,45 +31,32 @@ public class AppendBillLoader extends AbsAsyncTaskLoader {
             loaderResult.setFailMessageId(R.string.err_append_failed);
             loaderResult.setFailThrowable(new IllegalStateException("Can`t insert to unloaded bill list!"));
             loaderResult.setNotifyDataSet(false);
-            return loaderResult;
-        }
-
-        if(opResult == null) {
-            opResult = BillRepository.getInstance().insert();
-        }
-
-        if(opResult.isSuccessful()) {
-            long newBillId = opResult.getData();
-            Bill newBill = new Bill(newBillId);
-            BillList.getInstance().putModel(newBill);
-
-            if(loadOpResult == null) {
-                loadOpResult = BillOrderRepository.getInstance().loadGroupList(newBillId);
-            }
+        } else {
+            OperationResult<Long> opResult = BillRepository.getInstance().insert();
 
             if (opResult.isSuccessful()) {
-                List<BillOrder> billOrderList = loadOpResult.getData();
-                newBill.setModelList(billOrderList);
-                Bundle dataArgs = new Bundle();
-                dataArgs.putLong(BaseColumns._ID, newBillId);
-                loaderResult.setData(dataArgs);
-                loaderResult.setNotifyDataSet(true);
+                long newBillId = opResult.getData();
+                //Bill newBill = new Bill(newBillId);
+                //BillList.getInstance().putModel(newBill);
+                OperationResult<List<BillOrder>> loadOpResult = BillOrderRepository.getInstance().loadGroupList(newBillId);
+                loaderResult.setData(new Object[]{opResult, loadOpResult}); //TODO array of data
+                if (loadOpResult.isSuccessful()) {
+                    //List<BillOrder> billOrderList = loadOpResult.getData();
+                    //newBill.setModelList(billOrderList);
+                    loaderResult.setNotifyDataSet(true);
+                } else {
+                    loaderResult.setFailMessage(loadOpResult.getFailMessage());
+                    loaderResult.setFailMessageId(loadOpResult.getFailMessageId());
+                    loaderResult.setFailThrowable(loadOpResult.getFailThrowable());
+                    loaderResult.setNotifyDataSet(false);
+                }
             } else {
-                loaderResult.setFailMessage(loadOpResult.getFailMessage());
-                loaderResult.setFailMessageId(loadOpResult.getFailMessageId());
-                loaderResult.setFailThrowable(loadOpResult.getFailThrowable());
+                loaderResult.setFailMessage(opResult.getFailMessage());
+                loaderResult.setFailMessageId(opResult.getFailMessageId());
+                loaderResult.setFailThrowable(opResult.getFailThrowable());
                 loaderResult.setNotifyDataSet(false);
             }
-        } else {
-            loaderResult.setFailMessage(opResult.getFailMessage());
-            loaderResult.setFailMessageId(opResult.getFailMessageId());
-            loaderResult.setFailThrowable(opResult.getFailThrowable());
-            loaderResult.setNotifyDataSet(false);
         }
-
         return loaderResult;
     }
-
-    private OperationResult<Long> opResult;
-    private OperationResult<List<BillOrder>> loadOpResult;
 }
